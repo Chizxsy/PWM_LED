7d6d930bb3e5b62eee5cc1010b7c9d4dc6577337
Chizxsy <cfischer2247@gmail.com>
2025-03-09 23:18:47 -0700

    picamera2 instead of picamzero

--- irled_t.py
+++ irled_t.py
@@ -1,32 +1,32 @@
 from gpiozero import PWMLED
-from picamzero import Camera
+from picamera2 import Picamera2
 from time import sleep
 import os
 from datetime import datetime
 
-
-home_dir = os.environ['HOME'] #set the location of your home directory
+home_dir = os.environ['HOME']
 led = PWMLED(13)
-cam = Camera()
+cam = Picamera2()
+cam.configure(cam.create_still_configuration()) # Configure for still image capture
 
-def take_IR_photo(filename):
-    led.value = 1
-    cam.take_photo(filename) #save the image to your desktop
-    led.value = 0
+def take_photo_with_led_pulse(filename):
+    """Takes a photo and pulses the LED during the shutter period."""
+    led.value = 1  # Turn LED on at full brightness (start of shutter)
+    cam.start_and_capture_file(filename)
+    led.value = 0  # Turn LED off (end of shutter)
 
-try: 
+try:
     while True:
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
         filename = os.path.join(home_dir, f"photo_{timestamp}.jpg")
-        take_IR_photo(filename)
+        take_photo_with_led_pulse(filename)
         print(f"Photo taken: {filename}")
-        sleep(1)
-
+        sleep(1)  # Wait for 1 second
 
 except KeyboardInterrupt:
     print("Program terminated by user.")
-    led.close() # Cleanly close the led connection
-    cam.close() # Cleanly close the camera connection
+    led.close()
+    cam.close()
 
 except Exception as e:
     print(f"An error occurred: {e}")
