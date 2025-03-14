e967295f80f6b98a432afc5a0f242eb35386ecdf
Chizxsy <cfischer2247@gmail.com>
2025-03-13 18:36:14 -0700

    test

--- pwm.py
+++ pwm.py
@@ -1,11 +1,11 @@
-    import RPi.GPIO as GPIO
-    import time
+import RPi.GPIO as GPIO
+import time
 
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(18, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
 
-    pwm = GPIO.PWM(18, 50000)  # 5 kHz frequency
-    pwm.start(50)  # Start with 50% duty cycle
-    time.sleep(30)
-    pwm.stop()
-    GPIO.cleanup()
+pwm = GPIO.PWM(18, 50000)  # 5 kHz frequency
+pwm.start(50)  # Start with 50% duty cycle
+time.sleep(30)
+pwm.stop()
+GPIO.cleanup()
